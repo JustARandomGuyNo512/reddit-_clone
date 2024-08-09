14337fcb24e7793fa1feb14dbc3c141f5b2c9327
tjy1372@163.com <tjy1372@163.com>
2024-08-09 21:27:46 +0800

    完成登录登出

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,20 +1,65 @@
+"use client"
 import { FC } from "react";
 import { User } from "next-auth";
-import { DropdownMenu, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { signOut } from "next-auth/react";
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/DropdownMenu";
 import UserAvator from "./UserAvatar";
+import { DropdownMenuContent } from "./ui/DropdownMenu";
+import Link from "next/link";
 
 interface UserNavAccountProps {
-    user: Pick<User, "name" | "image"| "email">
+  user: Pick<User, "name" | "image" | "email">;
 }
 
-const UserAccountNav:FC<UserNavAccountProps> = ({user}) => {
-    return (
-        <DropdownMenu>
-            <DropdownMenuTrigger>
-                <UserAvator user={{name: user.name, image:user.image}} className="h-8 w-8"/>
-            </DropdownMenuTrigger>
-        </DropdownMenu>
-    )
-}
+const UserAccountNav: FC<UserNavAccountProps> = ({ user }) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <UserAvator
+          user={{ name: user.name, image: user.image }}
+          className="h-8 w-8"
+        />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="bg-white" align="end">
+        <div className="flex items-center justify-start gap-2 p-2">
+          <div className="flex flex-col space-y-1 leading-none">
+            {user.name && <p className="font-medium">{user.name}</p>}
+            {user.email && (
+              <p className="w-[200px] truncate text-sm text-muted-foreground">
+                {user.email}
+              </p>
+            )}
+          </div>
+        </div>
+        <DropdownMenuSeparator/>
+        <DropdownMenuItem asChild>
+            <Link href={"/"}>Feed</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+            <Link href={"/r/create"}>Create Community</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+            <Link href={"/settings"}>Settings</Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator/>
+        <DropdownMenuItem className='cursor-pointer' onSelect={
+            (e) => {
+                e.preventDefault();
+                signOut({
+                    callbackUrl: window.location.origin + "/sign-in",
+                });
+            }
+        }>
+            Sign-out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
 
-export default UserAccountNav
+export default UserAccountNav;
